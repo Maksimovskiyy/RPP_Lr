a02cf523c796836276d25345a73f119e490c06e1
Овчинников_Былин <movchinnikov00@mail.ru>
2023-02-25 15:10:28 +0700

    Сделали задания 1 и 2

---
+++ Lr2_1.1.py
@@ -0,0 +1,17 @@
+# Задание 1.1
+a = int(input('Введите первое число'))
+b = int(input('Введите второе число'))
+c = int(input('Введите третье число'))
+if b >= a <= c:
+    print(a)
+elif a >= b <= c:
+    print(b)
+elif a >= c <= b:
+    print(c)
+# Задание 1.2
+a = int(input('Введите первое число'))
+b = int(input('Введите второе число'))
+c = int(input('Введите третье число'))
+if a >= 1 and a <= 50:
+    print(a)
+elif
