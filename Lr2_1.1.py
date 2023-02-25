7f241b8def8b788b618c77c723c6444e94de8981
Овчинников_Былин <movchinnikov00@mail.ru>
2023-02-25 15:59:27 +0700

    Сделали задания 1 и 2

--- Lr2_1.1.py
+++ Lr2_1.1.py
@@ -8,10 +8,3 @@ elif a >= b <= c:
     print(b)
 elif a >= c <= b:
     print(c)
-# Задание 1.2
-a = int(input('Введите первое число'))
-b = int(input('Введите второе число'))
-c = int(input('Введите третье число'))
-if a >= 1 and a <= 50:
-    print(a)
-elif
---
+++ Lr2_1.2.py
@@ -0,0 +1,10 @@
+# Задание 1.2
+a = int(input('Введите первое число'))
+b = int(input('Введите второе число'))
+c = int(input('Введите третье число'))
+if a >= 1 and a <= 50:
+    print(a)
+if b >= 1 and b <= 50:
+    print(b)
+if c >= 1 and c <= 50:
+    print(c)
